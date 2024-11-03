b9ece3c3865ed3fc5d1635422daacc648da638bf
Stavros Avramidis <stavros9899@gmail.com>
2024-11-03 13:02:50 +0200

    [Lab2] Add plots for w&/o bind
    Signed-off-by: Stavros Avramidis <stavros9899@gmail.com>

--- lab2/kmeans/make_plots.py
+++ lab2/kmeans/make_plots.py
@@ -40,9 +40,64 @@ with open("./results/naive.out", "r") as f:
 
 # Create a DataFrame from the data
 df = pd.DataFrame(data)
+
+data = []
+with open("./results/naive_bind.out", "r") as f:
+    for line in f:
+        match = pattern.match(line)
+        if match:
+            data.append(
+                {
+                    "threads": int(match.group("threads")),
+                    "loops": int(match.group("loops")),
+                    "bind_total_time": float(match.group("total_time")),
+                    "bind_loop_time": float(match.group("loop_time")),
+                }
+            )
+
+df_bind = pd.DataFrame(data)
+
+# merge the two dataframes
+df = df.merge(df_bind, on=["threads", "loops"])
+
 df.sort_values(by=["threads"], inplace=True)
+
+# Calculate the speedup
+# df["speedup"] = df["loop_time"][0] / df["loop_time"]
+
 print(df)
 
+# Plot the results
 sns.barplot(data=df, x="threads", y="loop_time", color=next(bmh_colors))
+plt.xlabel("Number of threads")
+plt.ylabel("Time (s)")
+plt.title("Naive K-means")
 
-plt.savefig("./plots/naive.png")
+plt.savefig("./plots/naive.svg")
+plt.close()
+
+# Compare with and without binding
+
+df_melted = df.melt(
+    id_vars=["threads"],
+    value_vars=["loop_time", "bind_loop_time"],
+    var_name="Bind",
+    value_name="time",
+)
+df_melted['Bind'] = df_melted['Bind'].replace({'loop_time': 'Without', 'bind_loop_time': 'With'})
+
+sns.barplot(x='threads', y='time', hue='Bind', data=df_melted)
+plt.xlabel("Number of threads")
+plt.ylabel("Time (s)")
+plt.title("Comparison of With and Without Binding")
+plt.savefig("./plots/naive_bind.svg")
+plt.close()
+
+# plot the difference of bind and non-bind
+df['diff'] = df['loop_time'] - df['bind_loop_time']
+
+sns.barplot(data=df, x="threads", y="diff", color=next(bmh_colors))
+plt.xlabel("Number of threads")
+plt.ylabel("Time (s)")
+plt.title("Difference between With and Without Binding")
+plt.savefig("./plots/naive_bind_diff.svg")
