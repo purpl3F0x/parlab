cc64db29e32337a8b6270cb0ef6e383a4ff38fc9
Stavros Avramidis <stavros9899@gmail.com>
2025-02-12 16:24:44 +0200

    [Lab 4] Heat Transfer Initial commint and solutions
    Signed-off-by: Stavros Avramidis <stavros9899@gmail.com>

---
+++ lab4/heat_transfer/mpi/make_on_queue.sh
@@ -0,0 +1,18 @@
+# Give the Job a descriptive name
+#PBS -N make_heat_transfer_mpi
+
+## Output and error files
+#PBS -o make_heat_transfer_mpi.out
+#PBS -e make_heat_transfer_mpi.err
+
+## How many machines should we get?
+#PBS -l nodes=1:ppn=1
+
+##How long should the job run for?
+#PBS -l walltime=00:01:00
+
+## Start
+## Run make in the src folder
+
+cd $HOME/a4/heat_transfer/mpi
+make clean && make
---
+++ lab4/heat_transfer/mpi/run_jacobi.sh
@@ -0,0 +1,38 @@
+# Give the Job a descriptive name
+#PBS -N run_jacobi_mpi
+
+## Output and error files
+#PBS -o run_jacobi_mpi.out
+#PBS -e run_jacobi_mpi.err
+
+## How many machines should we get?
+#PBS -l nodes=8:ppn=8
+
+##How long should the job run for?
+#PBS -l walltime=00:20:00
+
+## Start
+## Load appropriate module
+module load openmpi/1.8.3
+
+
+# cd to the directory where the qsub command was issued
+cd $HOME/a4/heat_transfer/mpi
+
+# Clear the output file
+mkdir -p output
+> ./results/jacobi_mpi.out
+> ./results/jacobi_mpi_conv.out
+
+for size in 1024 2048 4096
+do
+    do
+        mpirun -np  1 --mca btl tcp,self ./jacobi_mpi $size $size 1 1 1>>./results/heat_transfer_mpi.out
+        mpirun -np  2 --mca btl tcp,self ./jacobi_mpi $size $size 2 1 1>>./results/heat_transfer_mpi.out
+        mpirun -np  4 --mca btl tcp,self ./jacobi_mpi $size $size 2 2 1>>./results/heat_transfer_mpi.out
+        mpirun -np  8 --mca btl tcp,self ./jacobi_mpi $size $size 4 2 1>>./results/heat_transfer_mpi.out
+        mpirun -np 16 --mca btl tcp,self ./jacobi_mpi $size $size 4 4 1>>./results/heat_transfer_mpi.out
+        mpirun -np 32 --mca btl tcp,self ./jacobi_mpi $size $size 8 4 1>>./results/heat_transfer_mpi.out
+        mpirun -np 64 --mca btl tcp,self ./jacobi_mpi $size $size 8 8 1>>./results/heat_transfer_mpi.out
+    done
+done
