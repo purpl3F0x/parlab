94d972a83d553bed0eaa505b215e02024b15a65a
Stavros Avramidis <stavros9899@gmail.com>
2025-02-12 16:27:02 +0200

    [Lab 2] Update results graphs
    Signed-off-by: Stavros Avramidis <stavros9899@gmail.com>

--- lab2/FW/make_plots.py
+++ lab2/FW/make_plots.py
@@ -24,7 +24,7 @@ fw_seq = pd.read_csv(os.path.join(METRICS_FOLDER, "fw_seq.csv")).rename(
 fw_sr = pd.read_csv(os.path.join(METRICS_FOLDER, "fw_sr.csv")).rename(
     columns=lambda x: x.strip()
 )
-fw_tiled = pd.read_csv(os.path.join(METRICS_FOLDER, "fw_tiled.csv")).rename(
+fw_tiled = pd.read_csv(os.path.join(METRICS_FOLDER, "fw_tiled_on_roids.csv")).rename(
     columns=lambda x: x.strip()
 )
 
@@ -87,6 +87,7 @@ for batch_size in fw_sr["BATCH_SIZE"].unique():
     )
     plt.close()
 
+
 # Plot 3: Tiled Floyd-Warshall Edition
 for batch_size in fw_tiled["BATCH_SIZE"].unique():
     plt.figure(figsize=(14, 7))
@@ -113,23 +114,32 @@ for batch_size in fw_tiled["BATCH_SIZE"].unique():
     subset = pd.concat([seq_data, subset], ignore_index=True)
 
     # Use colors from palette for hue
-    sns.barplot(
+    g = sns.barplot(
         data=subset, x="Thread_Label", y="TIME", hue="TILE_SIZE", palette=colors
     )
+    g.bar_label(g.containers[0], fontsize=12);
+    g.bar_label(g.containers[1], fontsize=12, rotation=90);
+    g.bar_label(g.containers[2], fontsize=12, rotation=90);
+    g.bar_label(g.containers[3], fontsize=12, rotation=90);
+    g.bar_label(g.containers[4], fontsize=12, rotation=90);
 
-    plt.title(
-        f"Tiled Floyd-Warshall Edition (Batch Size {batch_size})", fontsize=16
-    )  # Increase title font size
-    plt.xlabel("Threads", fontsize=14)  # Increase xlabel font size
-    plt.ylabel("Time (s)", fontsize=14)  # Increase ylabel font size
-    plt.legend(
-        title="Tile Size", loc="center left", bbox_to_anchor=(1, 0.5), fontsize=12
-    )  # Legend font size
-    plt.tick_params(
-        axis="both", which="major", labelsize=12
-    )  # Increase tick label size
-    plt.savefig(
-        os.path.join(PLOTS_FOLDER, f"Tiled_Floyd_Warshall_Batch_{batch_size}.svg"),
-        format="svg",
-    )
-    plt.close()
+
+
+
+
+plt.title(
+    f"Very-Very Fast Tiled Floyd-Warshall Edition (Batch Size {batch_size})", fontsize=16
+)  # Increase title font size
+plt.xlabel("Threads", fontsize=14)  # Increase xlabel font size
+plt.ylabel("Time (s)", fontsize=14)  # Increase ylabel font size
+plt.legend(
+    title="Tile Size", loc="center left", bbox_to_anchor=(1, 0.5), fontsize=12
+)  # Legend font size
+plt.tick_params(
+    axis="both", which="major", labelsize=12
+)  # Increase tick label size
+plt.savefig(
+    os.path.join(PLOTS_FOLDER, f"Tiled_Floyd_Warshall_Batch_{batch_size}.svg"),
+    format="svg",
+)
+plt.close()
